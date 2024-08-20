4b55fd0b7dbcb19ed9546a8bba4cbec5ba79daf9
drendog <dwenya@dren.dog>
2024-08-21 01:49:49 +0200

    feat: add re-exports

---
+++ src/reexports.rs
@@ -0,0 +1,12 @@
+pub use slint;
+pub use slint_interpreter;
+
+pub mod sctk {
+    pub use smithay_client_toolkit::reexports::*;
+}
+
+pub mod wayland_client {
+    pub use wayland_client::*;
+}
+
+pub use anyhow;
